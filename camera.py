9483adaf34290e1073c32d4fa75fe814811062be
Yumekawa_chan <20aj079@ms.dendai.ac.jp>
2023-01-09 00:34:22 +0900

    final update

--- camera.py
+++ camera.py
@@ -4,17 +4,15 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-num = 1
+print("Camera is operationing!")
 
 while True:
     dt_now = datetime.datetime.now()
+    ret,frame = cap.read()
     if dt_now.second == 0:
-        for i in range(10):
-            ret,frame = cap.read()
-        cv2.imwrite("./images/"+str(num)+".jpg",frame)
-        cap.release()
-        num += 1
-        print(num)
-        if num == 31:
-            num = 1
-        time.sleep(55)
+        cv2.imwrite(f"./images/{dt_now.hour:02}{dt_now.minute:02}.jpg",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    time.sleep(1)
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import discord
-import cv2
+import datetime
+import os
 
 TOKEN = 'MTA2MTMyODA4NTk1NTM4NzUxMw.GbOF8v.G_exgMmNavQ7BSv27UCYFtQSaxw6azzS7SGn_w'
-cap = cv2.VideoCapture(0)
 
 Intents = discord.Intents.default()
 Intents.members = True
+Intents.message_content = True
 client = discord.Client(intents=Intents)
 
 @client.event
@@ -14,12 +15,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == '彼氏':
-        for i in range(10):
-            ret,frame = cap.read()
-        cv2.imwrite("./images/img.jpg",frame)
-        cap.release()
-        await message.channel.send(file=discord.File("./images/img.jpg"))
-        
+    if message.content == '/now':
+        dt_now = datetime.datetime.now()
+        await message.channel.send(file=discord.File(f"./images/{dt_now.hour:02}{dt_now.minute:02}.jpg"))
+    if message.content[:5] == '/past':
+        tmp_time = message.content[5:9]
+        if not (f"{tmp_time}.jpg" in os.listdir(path='./images/')):
+            await message.channel.send('Your text might be incorrect.')
+        else:
+            await message.channel.send(file=discord.File(f"./images/{message.content[5:9]}.jpg"))
 
 client.run(TOKEN)
